bd98e5332e39d0b53c05328ac8ef7b8053aa2e73
markwasfy <markwasfy00@gmail.com>
2023-09-18 04:37:04 +0200

    new

--- Media-Source-refresh-script/main.py
+++ Media-Source-refresh-script/main.py
@@ -1,9 +1,8 @@
 import obsws_python as obs
-import schedule
 import time
 import toml
-import sys
-
+import datetime
+import pytz
 
 data = toml.load("./config.toml")
 
@@ -12,39 +11,48 @@ REFRESH_CYCLE = data["settings"]["refresh_time"]
 OBS_HOST = data["obs"]["host"]
 OBS_PORT = data["obs"]["port"]
 OBS_PASS = data["obs"]["password"]
-CONNECTION = False
+
+CHECKED = False
+
 
 class Obs:
     HOST = OBS_HOST
     PORT = OBS_PORT
     PASSWORD = OBS_PASS
+    CONNECTION = False
 
-    
-    def __init__(self):
+    def start(self):
         self.cl = obs.ReqClient(host=self.HOST, port=self.PORT, password=self.PASSWORD)
+        self.CONNECTION = True
 
     def refresh_media_source(self):
-        global CONNECTION
-        if CONNECTION:
-            try:
-                self.cl.set_input_settings(REFRESH_SOURCE_NAME, {},True)
-                print("media source refreshed!")
-            except:
-                print("failed to refresh...")
-                CONNECTION = False
-                sys.exit()
+        self.cl.set_input_settings(REFRESH_SOURCE_NAME, {},True)
+        print("media source refreshed!")
+            
 
-while not CONNECTION:
-    try:
-        obs = Obs()
-        schedule.every(REFRESH_CYCLE).minutes.do(obs.refresh_media_source)
-        CONNECTION = True
-        print("script running...")
-    except:
-        print("Please open obs")
-        print("Retrying again after 10 seconds")
-        time.sleep(10)
+TP = Obs()
 
 while True:
-    schedule.run_pending()
+    if not TP.CONNECTION:
+        try:
+            TP.start()
+            print("Connected")
+        except:
+            print("Failed to connect retrying after 10 secs")
+            time.sleep(10)
+    else:
+        try:
+            israel_tz = pytz.timezone("Israel")
+            now = datetime.datetime.now(israel_tz)
+            if (now.minute == 0 or now.minute == 30) and not CHECKED:
+                TP.refresh_media_source()
+                CHECKED = True
+            elif (now.minute == 0 or now.minute == 30) and CHECKED:
+                pass
+            else:
+                CHECKED = False
+        except:
+            TP.CONNECTION = False
+            print("Connection dropped, trying again")
     time.sleep(1)
+    
